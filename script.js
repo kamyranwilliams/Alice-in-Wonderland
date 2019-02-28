2ec47d5183dcc046d71bda599e13f03f591f8f7c
kamyranwilliams <37843545+kamyranwilliams@users.noreply.github.com>
2019-02-28 08:45:13 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,172 +1,92 @@
-let moves = 0;
-let counter = document.querySelector(".moves");
-// declaring variable of matchedCards
-let matchedCard = document.getElementsByClassName("match");
- // close icon in modal
- let closeicon = document.querySelector(".close");
-// declare modal
- let modal = document.getElementById("popup1")
+// el.src.match("images/lemon_slot.PNG")
+// el.src = "images/orange_slot.PNG";
+// 
+// document.getElementById('you-win').play();
+// el.src.match("images/cherry_slot.PNG")
+// el.src = "images/lemon_slot.PNG";
+// el.src.match("images/orange_slot.PNG")
+a = document.getElementById("slot1");
+b = document.getElementById("slot2");
+c = document.getElementById("slot3");
+var mySound = document.getElementById('you-win');
+// cycle images in the following order: CHERRY --> LEMON --> ORANGE
+function nextImage(el){
+	if (el.src.match("images/cherry_slot.PNG")){
+		
+		el.src = "images/lemon_slot.PNG";
+		
+	} else if (el.src.match("images/lemon_slot.PNG")){
+		
+		el.src = "images/orange_slot.PNG";
+		
+	} else if (el.src.match("images/orange_slot.PNG")){
+		
+	el.src = "images/cherry_slot.PNG";
+	
+	} else {
+		el.src = "images/cherry_slot.PNG";
+	}
+}
 
- // array for opened cards
-var openedCards = [];
-// cards array holds all cards
-let card = document.getElementsByClassName("card");
-let cards = [...card]
-
-// deck of all cards in game
-const deck = document.getElementById("card-deck");
-document.body.onload = startGame();
-// @description function to start a new play 
-function startGame(){
-    // shuffle deck
-    cards = shuffle(cards);
-    // remove all exisiting classes from each card
-    for (var i = 0; i < cards.length; i++){
-        deck.innerHTML = "";
-        [].forEach.call(cards, function(item) {
-            deck.appendChild(item);
-        });
-        cards[i].classList.remove("show", "open", "match", "disabled");
-    }
-    // reset moves
-    moves = 0;
-    counter.innerHTML = moves;
-     //reset timer
-    second = 0;
-    minute = 0; 
-    hour = 0;
-    var timer = document.querySelector(".timer");
-    timer.innerHTML = "0 mins 0 secs";
-    clearInterval(interval);
-}
-// @description toggles open and show class to display cards
-var displayCard = function (){
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-    this.classList.toggle("disabled");
-};
-function cardOpen() {
-    openedCards.push(this);
-    var len = openedCards.length;
-    if(len === 2){
-        moveCounter();
-        if(openedCards[0].type === openedCards[1].type){
-            matched();
-        } else {
-            unmatched();
-        }
-    }
-};
-
-function matched(){
-    openedCards[0].classList.add("match", "disabled");
-    openedCards[1].classList.add("match", "disabled");
-    openedCards[0].classList.remove("show", "open", "no-event");
-    openedCards[1].classList.remove("show", "open", "no-event");
-    openedCards = [];
-}
-function unmatched(){
-    openedCards[0].classList.add("unmatched");
-    openedCards[1].classList.add("unmatched");
-    disable();
-    setTimeout(function(){
-        openedCards[0].classList.remove("show", "open", "no-event","unmatched");
-        openedCards[1].classList.remove("show", "open", "no-event","unmatched");
-        enable();
-        openedCards = [];
-    },1100);
-}
-function disable(){
-    Array.prototype.filter.call(cards, function(card){
-        card.classList.add('disabled');
-    });
-}function enable(){
-    Array.prototype.filter.call(cards, function(card){
-        card.classList.remove('disabled');
-        for(var i = 0; i < matchedCard.length; i++){
-            matchedCard[i].classList.add("disabled");
-        }
-    });
-}function moveCounter(){
-    moves++;
-    counter.innerHTML = moves;
-    //start timer on first click
-    if(moves == 1){
-        second = 0;
-        minute = 0; 
-        hour = 0;
-        startTimer();
-    }
-    // setting rates based on moves
-    if (moves > 8 && moves < 12){
-        for( i= 0; i < 3; i++){
-            if(i > 1){
-                stars[i].style.visibility = "collapse";
-            }
-        }
-    }
-    else if (moves > 13){
-        for( i= 0; i < 3; i++){
-            if(i > 0){
-                stars[i].style.visibility = "collapse";
-            }
-        }
-    }
-}
-var second = 0, minute = 0; hour = 0;
-var timer = document.querySelector(".timer");
-var interval;
-function startTimer(){
-    interval = setInterval(function(){
-        timer.innerHTML = minute+"mins "+second+"secs";
-        second++;
-        if(second == 60){
-            minute++;
-            second=0;
-        }
-        if(minute == 60){
-            hour++;
-            minute = 0;
-        }
-    },1000);
-}
-function congratulations(){
-    if (matchedCard.length == 16){
-        clearInterval(interval);
-        finalTime = timer.innerHTML;
-
-        // show congratulations modal
-        modal.classList.add("show");
-
-      
-        //showing move, rating, time on modal
-        document.getElementById("finalMove").innerHTML = moves;
-       
-        document.getElementById("totalTime").innerHTML = finalTime;
-
-        //closeicon on modal
-        closeModal();
-    };
-}
-// @description close icon on modal
-function closeModal(){
-    closeicon.addEventListener("click", function(e){
-        modal.classList.remove("show");
-        startGame();
-    });
-}
-// @desciption for user to play Again 
-function playAgain(){
-    modal.classList.remove("show");
-    startGame();
+// LEMON --> BAR --> CHERRY
+// function nextImage(el){}
+function nextImage2(el){
+	if (el.src.match("images/lemon_slot.PNG")){
+		
+		el.src = "images/bar_slot.PNG";
+		
+	} else if (el.src.match("images/bar_slot.PNG")){
+		
+		el.src = "images/cherry_slot.PNG";
+		
+	} else if (el.src.match("images/cherry_slot.PNG")){
+		
+	el.src = "images/lemon_slot.PNG";
+	
+	} else {
+		el.src = "images/lemon_slot.PNG";
+	}
 }
 
 
-// loop to add event listeners to each card
-for (var i = 0; i < cards.length; i++){
-    card = cards[i];
-    card.addEventListener("click", displayCard);
-    card.addEventListener("click", cardOpen);
-    card.addEventListener("click",congratulations);
-};
+// ORANGE --> CHERRY --> SEVEN --> WATERMELON
+//function nextImage(el){}
+function nextImage3(el){
+	if (el.src.match("images/orange_slot.PNG")){
+		
+		el.src = "images/cherry_slot.PNG";
+		
+	} else if (el.src.match("images/cherry_slot.PNG")){
+		
+		el.src = "images/seven_slot.PNG";
+		
+	} else if (el.src.match("images/seven_slot.PNG")){
+		
+	el.src = "images/watermelon_slot.PNG";
+	
+	}
+	else if (el.src.match("images/watermelon_slot.PNG")){
+		
+	el.src = "images/orange_slot.PNG";
+	
+	} else {
+		el.src = "images/orange_slot.PNG";
+	}
+}
 
+function playSound() {
+	// when the JACKPOT! button is clicked, play the slot machine sound
+	document.getElementById('you-win').play();
+}
+
+function checkSlot(el){
+	if (a.src.match("cherry_slot") && b.src.match("cherry_slot") && c.src.match("cherry_slot")){
+		mySound.play();
+		a.classList.add("opaque");
+		b.classList.add("opaque");
+		c.classList.add("opaque");
+		console.log("YOU WIN");
+
+	} else{
+	}
+}
